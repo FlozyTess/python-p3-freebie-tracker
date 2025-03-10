fe61d207075898462de9c29c7d42d26eb54339d7
FlozyTess <cecilrency23@gmail.com>
2025-03-10 16:12:49 +0300

    deleted check test

--- lib/check_test.py
+++
@@ -1,8 +0,0 @@
-from models import session, Company, Dev, Freebie
-
-# Test Company creation
-company = Company(name="Google", founding_year=1998)
-session.add(company)
-session.commit()
-
-print(session.query(Company).all())  # returns the company object
