797966f04faf5b0377602324c68befa3f60d68b0
FlozyTess <cecilrency23@gmail.com>
2025-03-10 16:20:02 +0300

    updated debug.py

--- lib/debug.py
+++ lib/debug.py
@@ -6,22 +6,22 @@ from models import session,Company, Dev,Freebie
 
 if __name__ == '__main__':
     engine = create_engine('sqlite:///freebies.db')
-    #import ipdb; ipdb.set_trace()
-    # Test Dev retrieval
+    import ipdb; ipdb.set_trace()
+    # Test Dev 
     njeri = session.query(Dev).filter_by(name="Njeri").first()
     if njeri:
         print(njeri.companies)
     else:
         print("Developer Njeri not found.")
 
-    # Test Company retrieval
+    # Test Company 
     techSolutions = session.query(Company).filter_by(name="TechSolutions").first()
     if techSolutions:
         print(techSolutions.devs)
     else:
         print("Company TechSolutions not found.")
 
-    # Test Freebie retrieval
+    # Test Freebie 
     freebie = session.query(Freebie).filter_by(item_name="T-Shirt").first()
     if freebie:
         print(freebie.print_details())
